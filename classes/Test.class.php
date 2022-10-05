a8dc13c7d123df538a86c31391a02e02f5a470de
DemRossi <wesleywijsen@hotmail.com>
2022-10-06 00:18:40 +0200

    Setup and testing

---
+++ classes/Test.class.php
@@ -0,0 +1,9 @@
+<?php 
+    class Test{
+        private $test = "Hello World!";
+
+        public function getTest(){
+            return $this->test;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php 
-    $test = "Hello World!!";
+    //Connectie classes
+    require_once 'bootstrap/bootstrap.php';
+
+    $test = new Test();
+    $text = $test->getTest();
+    
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nasa</title>
+    <?php include_once 'includes/head.inc.php'; ?>
 </head>
 <body>
-    <h1><?php echo $test; ?></h1>
+    <h1><?php echo $text; ?></h1>
 </body>
 </html>
